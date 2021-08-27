0141a1f3c1c7cec522227521c94b82d4853656ef
Aritra Mondal <mondalaritra002@gmail.com>
2021-08-28 02:01:29 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"github.com/dgrijalva/jwt-go"
+ 	"github.com/gin-gonic/gin"
+ 	"github.com/go-redis/redis/v7"
+  	"github.com/twinj/uuid"
+
+  	"log"
+  	"net/http"
+  	"os"
+  	"time"
+  	"strconv"
+)
+
+type User struct {
+	ID uint64       `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type TokenDetails struct {
+	AccessToken  string
+	RefreshToken string
+	AccessUuid   string
+	RefreshUuid  string
+	AtExpires    int64
+	RtExpires    int64
+}
+
+//A sample use
+var user = User{
+	ID:          1,
+	Username: "username",
+	Password: "password",
+}
+
+var (
+	router = gin.Default()
+)
+
+func main() {
+	router.POST("/login", Login)
+	log.Fatal(router.Run(":8080"))
+}
+
+func Login(c *gin.Context) {
+	var u User
+  	if err := c.ShouldBindJSON(&u); err != nil {
+    	c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+     	return
+  	}
+  	//compare the user from the request, with the one we defined:
+  	if user.Username != u.Username || user.Password != u.Password {
+    	c.JSON(http.StatusUnauthorized, "Please provide valid login details")
+     	return
+  	}
+  	ts, err := CreateToken(user.ID)
+ 	if err != nil {
+ 		c.JSON(http.StatusUnprocessableEntity, err.Error())
+   		return
+	}
+ 	saveErr := CreateAuth(user.ID, ts)
+  	if saveErr != nil {
+    	c.JSON(http.StatusUnprocessableEntity, saveErr.Error())
+  	}
+  	tokens := map[string]string{
+    	"access_token":  ts.AccessToken,
+    	"refresh_token": ts.RefreshToken,
+  	}
+  	c.JSON(http.StatusOK, tokens)
+}
+
+func CreateToken(userid uint64) (*TokenDetails, error) {
+	td := &TokenDetails{}
+	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.AccessUuid = uuid.NewV4().String()
+
+	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+	td.RefreshUuid = uuid.NewV4().String()
+
+	var err error
+	//Creating Access Token
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["access_uuid"] = td.AccessUuid
+	atClaims["user_id"] = userid
+	atClaims["exp"] = td.AtExpires
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	td.AccessToken, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+	 return nil, err
+	}
+	//Creating Refresh Token
+	os.Setenv("REFRESH_SECRET", "mcmvmkmsdnfsdmfdsjf") //this should be in an env file
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUuid
+	rtClaims["user_id"] = userid
+	rtClaims["exp"] = td.RtExpires
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+	return td, nil
+}
+
+
+var  client *redis.Client
+
+func init() {
+  	//Initializing redis
+  	dsn := os.Getenv("REDIS_DSN")
+  	if len(dsn) == 0 {
+    	dsn = "localhost:6379"
+  	}
+  	
+  	client = redis.NewClient(&redis.Options{
+    	Addr: dsn, //redis port
+  	})
+  	_, err := client.Ping().Result()
+  	
+  	if err != nil {
+    	panic(err)
+  	}
+}
+
+func CreateAuth(userid uint64, td *TokenDetails) error {
+ 	at := time.Unix(td.AtExpires, 0) //converting Unix to UTC(to Time object)
+ 	rt := time.Unix(td.RtExpires, 0)
+ 	now := time.Now()
+
+ 	errAccess := client.Set(td.AccessUuid, strconv.Itoa(int(userid)), at.Sub(now)).Err()
+ 	if errAccess != nil {
+  		return errAccess
+ 	}
+ 	errRefresh := client.Set(td.RefreshUuid, strconv.Itoa(int(userid)), rt.Sub(now)).Err()
+ 	if errRefresh != nil {
+  		return errRefresh
+ 	}
+	return nil
+}
